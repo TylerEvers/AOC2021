558d76a53a817f0d57950512a05d29ee514a58f6
Tyler Evers <TEves2015@gmail.com>
2021-12-25 21:32:02 -0600

    Add project files.

---
+++ AOC2021/Day1/Day1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class AOCDay1
+{
+    public static void Day1()
+    {
+        var lstDepths = ReadFile();
+        CalculateIncreases(lstDepths);
+    }
+
+    private static void CalculateIncreases(List<int> lstDepths)
+    {
+        var intIncreasesCount = 0;
+        var intPrev = lstDepths[0];
+
+        foreach (int depth in lstDepths.Skip(1))
+        {
+            if (depth > intPrev)
+            {
+                intIncreasesCount++;
+            }
+
+            intPrev = depth;
+        }
+
+        Console.WriteLine($"Answer: {intIncreasesCount}");
+    }
+
+    public static List<int> ReadFile()
+    {
+        List<int> lstDepths = new List<int>();
+
+        lstDepths = System.IO.File.ReadAllLines(@"..\Day1.txt").Select(int.Parse).ToList();
+
+        return lstDepths;
+    }
+}
+
---
+++ AOC2021/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AOC2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code - 2021!");
+
+            Console.WriteLine("Day 1");
+            AOCDay1.Day1();
+        }
+    }
+}
